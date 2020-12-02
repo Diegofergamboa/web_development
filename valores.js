b2b381e6bf582202023f6e5ac4ee93c36e629db6
Diego Ortiz Gamboa <diegofergamboa@hotmail.es>
2020-12-02 14:39:58 -0500

    Aqui conociendo como funciona en promedio Javascript y como ir aprendiendo de una mejor manera, siendo referente por ejemplo el navegador

---
+++ valores.js
@@ -0,0 +1,28 @@
+
+'VALORES PRIMITIVOS'
+
+'Las comillas son importantes para generar string'
+'Es decir son cadenas de texto para que el usuario pueda verlo'
+
+40
+
+'Valor booleano. Dentro del lenguaje nos permite validar ciertas condiciones o cosas en general'
+true
+false
+
+'Empty values. SOn valores reservados, nos permiten guardar en memoria, es decir generar el espacio reservado'
+
+null
+undefined 
+
+'VALORES NO PRIMITIVOS'
+
+'Un valor de tipo objeto'
+
+[1, 2, 3]
+
+'Es un valor de tipo objeto de igual manera'
+
+{nombre: "Diego"}
+
+'La funcion "type" es muy importante'
