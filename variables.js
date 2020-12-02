60ff87209cc41b348a0722d9de6838aca226e3eb
Diego Ortiz Gamboa <diegofergamboa@hotmail.es>
2020-12-02 14:56:45 -0500

    Se asignaron valores en memoria a travès de variables, se miro como traerlas a imprimir en consola y, demas

---
+++ variables.js
@@ -0,0 +1,39 @@
+
+'Aprender que son las variables y como aprender sobre ellas'
+'Una variable es la representaciòn de algun lugar en memoria en el que vamos a almacenar cosas en memoria'
+
+
+'Var es la palabra reservada para la creaciòn de variables, el espacio en la memoria se llamada "nombre"'
+'En vez de poner el nombre de Diego, se coloca "nombre"'
+'EL punto y coma es de buenas practicas indicando que terminamos de colocar una sentencia'
+'La manera de guardar los datos siempre va a ser por medio de variables'
+
+
+'ESTADOS DE UNA VARIABLE'
+
+// Con esto comentamos en JS
+
+
+var nombre = "Diego"; 
+
+// estoy declarando una variable diciendo "por favor reserva el espacio"
+var edad;
+
+// inicializar
+edad = 30; // es decir colocarle un valor a la variable
+
+
+// Los arrays son valores similares a una lista 
+
+var elementos = ["Computador", "Celular"]
+
+var persona = {
+    nombre: "Diego", // Es una variable compleja
+    edad: 24
+}
+
+persona;
+
+// Para colocar los elementos lo que debemos hacer es simple solo es colocar el nombre de la variable
+// , encerrar en corcchetes [] la asignaciòn que tiene el elemento en memoria, conociendo que 
+//, es importante que en programaciòn siempre se comienza con 0 a infinito
