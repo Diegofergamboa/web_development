912ebe54531c62f6fe876ce4f1867cfd0616c6fa
Diego Ortiz Gamboa <diegofergamboa@hotmail.es>
2020-12-02 15:27:26 -0500

    Se conocieron los dos tipos de funciones, declarativas y de expresiòn, siendo las primeras las referentes a variables que se sacan con su respectiva palabra que es function, seguido de las variables expresiòn, las cuales son funciones que se almacenan e inicializan dentro de una variable

---
+++ funciones.js
@@ -0,0 +1,38 @@
+'Las funciones son un conjunto de sentencia'
+'Es de cir es un conjunto de sentencias con el fin de generar o realizar otras cosas'
+
+ // FUNCIONES DECLARATIVAS
+
+function miFuncion() {
+    return 3;
+}
+miFuncion();
+// Va a inicializar en la memoria un numero
+
+ // FUNCIONES DE EXPRESIÒN
+
+ // Se inicializan por medio de variables.
+
+ var miFuncion = function(a,b){
+     return a + b ;
+}
+miFuncion();
+
+
+'EL console log nos permite imprimir por consola'
+'Plantillas de cadenas de texto es ligar variables'
+
+'''
+    saludarEStudiantes("Diego"){
+        console.log(`Hola ${estudiante}`); // Este proceso es conocido como 
+    }
+'''
+
+'''
+function multiplicacion(x,y){
+    var resultado = x * y ;
+    return `El resultado de ${x} multiplicado por ${y} es igual a ${resultado}` ; 
+  }
+      
+  multiplicacion(1000, 2);
+'''
