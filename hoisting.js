9a9e9f23563995764cf4ddb1f1b94f22c66ee631
Diego Ortiz Gamboa <diegofergamboa@hotmail.es>
2020-12-03 22:51:47 -0500

    Usar condicionantes

---
+++ hoisting.js
@@ -0,0 +1,24 @@
+// 'Solo pasa con EMCscript 5 hacia abajo'
+
+// Las nuevas versiones presentan las variables const y len
+
+// Ir al curso de EMCscript 6.
+
+// Ejemplos de como generar un hoisting y no permitir que esto dañe el codigo
+
+// Nosotros tenemos dos estados para tener una variable
+
+console.log(miNombre) // Es una funcion que nos permite imprimir los resultados de todo lo que llevamos haciendo
+
+
+var miNombre;
+// Se declara
+miNombre = "Diego ";
+// Se inicializa
+
+// El hoisting es que se esta mandando a llamar una variable cuando no esta creada
+
+
+// Para declarar las funciones debo declararlas al principio de los Scripts
+
+
---
+++ if.js
@@ -0,0 +1,23 @@
+/* Cubiculo de votaciòn */
+
+var edad = 18;
+if (edad === 18) {
+    console.log("Felicitaciones es tu primera votaciòn");
+} else if (edad > 18) {
+    console.log("Es probable que esta sea tu segunda votaciòn");
+} else {
+    console.log("Muy posiblemente eres menor de edad, lo que no te permite votar");
+}
+
+/* Otra de las formas de generar condicionantes es una sola linea de codigo, las siguientes dos lineas pertenecen a la sintaxis 
+y, a un ejercicio pràctico */
+
+/* Es importante inicializar precisamente las condiciones por medio de un variable */
+
+
+condition ? true : false
+
+var real = 5
+
+var comprobacion = real > 0 ? `${real} + es un numero positivo` : `${real} + es un numero negativo`;
+
