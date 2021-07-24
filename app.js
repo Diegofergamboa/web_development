03a5eee696acbb65563391e5016ec84b64b6a90b
Diegofergamboa <diegofergamboa@hotmail.es>
2021-07-24 14:12:06 -0500

    firs steps studying good practices and the importance for the order of the code, some scopes from the emc6

---
+++ app.js
@@ -0,0 +1,11 @@
+// alert("Alerta")
+
+console.log("variables")
+
+var name = 'agustin' ;
+console.log(name)
+
+// Es importante de que el orden del código sea acorde con primero la declaración de variables seguido de la 
+// declaración de y ejecución de variables, porque aunque JS sea dinámico, es importante tener buenas prácticas
+// para que el desarrollador nos entienda a la perfección
+
