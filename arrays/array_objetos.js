838d1c1850e7f6ba531a243a9a5c0310f536e503
Diegofergamboa <diegofergamboa@hotmail.es>
2021-07-29 08:54:21 -0500

    Se estudian que son los array que pueden tener objetos y objetos con arrays adentro y, se hacen ejercicios de operaciones con arrays

---
+++ arrays/array_objetos.js
@@ -0,0 +1,19 @@
+// Las arrays de objetos, son basicamente objetos que pueden estar dentro de arrays
+
+let superheroes = [
+    {
+        name : "Superman" ,
+        superpoderes : ["Super fuerza", "Volar"]
+    } ,
+    {
+        name: "Linterna verde" ,
+        superpoderes: ["Volar", "Rayo láser"]
+    } ,
+    {
+        batman: "Batman" ,
+        superpoderes: ["Dinero", "Traje"]
+    }
+] ;
+
+console.log(superheroes[2])
+
