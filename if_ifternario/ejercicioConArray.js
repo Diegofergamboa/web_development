012d3d15436b02cb97d1b518c974f0d9afee1132
Diegofergamboa <diegofergamboa@hotmail.es>
2021-07-29 16:26:08 -0500

    Se estudian las posibles aplicaciones de if y de if ternario en un ejemplo y, se estudian de nuevos propiedades del array

--- if_ifternario/ejercicioConArray.js
+++ if_ifternario/ejercicioConArray.js
@@ -1,34 +1,92 @@
 
 /* Creación del Array */ 
 
-let OperadoresColombia [
+let OperadoresColombia = [
     {
-        nombre : "" ,
+        nombre : "movistar" ,
         conexion : {
-            rural ,
-            urbana ,
-        }
-        teléfonos = []
+            rural : "3G",
+            urbana: "4G",
+        } ,
+        telefonos : ["motorola", "apple"] ,
+        cincoG : true                 
     } ,
     {
-        nombre : "" ,
+        nombre : "claro" ,
         conexion : {
-            rural ,
-            urbana ,
-        }
-        teléfonos = []
+            rural : "2G",
+            urbana : "3G",
+        } ,
+        telefonos : ["apple", "huawei"] ,
+        cincoG : false   
     } ,
     {
-        nombre : "" ,
+        nombre : "wom" ,
         conexion : {
-            rural ,
-            urbana ,
-        }
-        teléfonos = []
-    } ,
-    
+            rural : "No tiene" ,
+            urbana: "4G" ,
+        } ,
+        telefonos : ["alcatel"] ,
+        cincoG : false  
+    }
 ] ;
 
+
+
+
+/* Imprimir, el primer elemento y, la última tienen conexión rural de tanto y conexión urbana de tanto */
+
+console.log(`${OperadoresColombia[0].nombre} tiene una conexión rural de ${OperadoresColombia[0].conexion.rural} y urbana de ${OperadoresColombia[0].conexion.urbana}`)
+
 /* Agregar, quitar (inicio y al final) - */ 
-/* Clonar en servicio de internet con banda ancha true o false */
+
+// upload at end
+
+OperadoresColombia.push(
+    {
+        nombre: "telcel" ,
+        conexion : {
+            rural: "No tiene" ,
+            urbana: "2G"
+        },
+        telefonos : ["alcatel"] ,
+        cincoG : true   
+    }
+)
+
+
+// upload at begin
+
+OperadoresColombia.unshift(
+    {
+    nombre : "tigo" ,
+    conexion : {
+        rural : "2G" ,
+        urbana: "4G"
+    } ,
+    telefonos : ["alcatel", "xiaomi"] , 
+    cincoG : false   
+    }
+)
+
+// erase at begin
+
+// OperadoresColombia.shift() 
+
+// era at finish
+
+// OperadoresColombia.pop()
+
+/* Clonar en servicio de internet con cincoG true o false */
+
+let OperadoresColombiaInternet = OperadoresColombia.slice() 
+
+
 /* Con los servicio de internet voy a hacer las validaciones de if y if_tenario */
+
+if (OperadoresColombiaInternet[0]) {
+    console.log(`${OperadoresColombiaInternet[0].nombre} aplicará tecnología 5G"`) ;
+} else {
+    console.log("No aplica tecnología 5G") ;
+}
+
