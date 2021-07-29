42a542c116d9086f36ad685035f6325acbb30710
Diegofergamboa <diegofergamboa@hotmail.es>
2021-07-29 14:55:41 -0500

    Se estudia la forma convencional del condicional if

---
+++ if_ifternario/if.js
@@ -0,0 +1,12 @@
+
+// Los condicionales son basicamente una forma de que si un argumento es verdadero o falso, este a a rrojar un resultado o un none dpeendiende de lo que quiera el woftware witer
+
+let a = 1 ;
+
+if ( a === 1 ) { //La primera validación //Se usan los tres iguales para decir que debe ser exactamente igual, (EXACTOOO!)
+    console.log("a es igual a 1") 
+} else if (a != 1) { // else if se usa para evaluar otra condición
+    console.log("a es diferente que 1") 
+} else { // se usa para la última validación
+    console.log("a no es un numero") 
+}
