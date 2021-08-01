592a1fcbea582cef3cec4829af5c1fbd4e4de62d
Diegofergamboa <diegofergamboa@hotmail.es>
2021-08-01 17:02:11 -0500

    Se realiza ejemplo de como construir objetos e interactuar con ellos a través de funciones

--- functions/functions2.js
+++ functions/functions2.js
@@ -1,1 +1,32 @@
-alert("funciones con objetos")
+// El objetivo es llamar una función que se le pase como parámetro un objeto ya establecido
+
+// Contrucción del objeto = 
+
+let singleCar = [ 
+    { 
+    casa: "BMW" ,
+    modelo: "1995" ,
+    marca: "M3"
+    } ,
+    { 
+    casa: "Mercedes Benz" ,
+    modelo: "2020" ,
+    marca: "AM5"
+    },
+    { 
+    casa: "Ford" ,
+    modelo: "2006" ,
+    marca: "Mustang GT"
+    }
+] ;
+// console.log(singleCar.marca)
+
+function Car (singleCar) {
+    for (let i = 0 ; i <= singleCar.lenght ; i ++ ) 
+    {
+        console.log(i) ;
+        console.log(`Automovil de casa ${singleCar[i].casa}, modelo ${singleCar[i].modelo} y, marca ${singleCar[i].marca}`) ;
+    }
+} ;
+
+Car() ;
