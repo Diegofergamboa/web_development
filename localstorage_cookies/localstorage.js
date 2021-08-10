012b0d00c821405feaf3d3914a21378e96a3f179
Diegofergamboa <diegofergamboa@hotmail.es>
2021-08-10 14:52:48 -0500

    Se revisa localstorage, crear keys con values y, modificarlas

--- localstorage_cookies/localstorage.js
+++ localstorage_cookies/localstorage.js
@@ -1,3 +1,5 @@
+alert("Localstorage");
+
 var nombre = "Diego" ;
 var apellido = "Ortiz" ;
 var ciudad = "Pamplona" ;
@@ -5,3 +7,18 @@ var ciudad = "Pamplona" ;
 localStorage("nombre",nombre) ;
 localStorage("nombre",apellido) ;
 localStorage("nombre",ciudad) ;
+
+console.log(localStorage.getItem("nombre"));
+console.log(localStorage.getItem("apellido"));
+console.log(localStorage.getItem("ciudad"));
+
+
+// Para remover los valores del local storage
+
+localStorage.removeItem("apellido");
+
+// Para actualizar los datos 
+
+localStorage.setItem("nombre","Fernando") ;
+
+// Es bueno dominar localstorage, con el fín de poder interactuar mejor con las API`s y la información que allí se guarda
