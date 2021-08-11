4cb31d79072d02ff5708ed61eba869d68c721cb4
Diegofergamboa <diegofergamboa@hotmail.es>
2021-08-11 15:03:33 -0500

    Hacer un evento de enviar formulario para evitar que se recargue la página y no guarde la información

---
+++ event_phrom/app.js
@@ -0,0 +1,5 @@
+
+function enviarFormulario () {
+    event.preventDefault(); // Aqui le digo al código que cuando pase el evento de que se envie el formulario nos envie el console.log
+    console.log('Formulario enviado');
+};
