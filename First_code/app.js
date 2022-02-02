df4125c41509336e5ee189516f6137a23f65cb13
Diego Fernando Ortiz Gamboa <72028938+Diegofergamboa@users.noreply.github.com>
2022-02-01 21:02:10 -0500

    Update app.js

--- First_code/app.js
+++ First_code/app.js
@@ -1,19 +1,16 @@
-var variable = "Agustin" ;
-var variable = 19 ;
-var variable = {} ;
-var variable = 15.2 ;
+// Dividir palabra en letras dentro de un array
+// Variable con palabra al reves
+// return final de comprobacion de si son iguales las palabras
 
-
-function tipovariable (variable) {
-    return console.log(`El tipo de variable para ${variable}, es de ${typeof(variable)}`) ;
-    debugger
+function palindromeComprobation(newWord) {
+	// Convertir el string a array
+	const separator = (newWord=> {
+		return newWord.split('');
+	});
+	const arrayWord = separator(newWord);
+	console.log(arrayWord[arrayWord.length-1]);
+	// Variable con palabra al revés
+	//console.log(asunderWord);
 }
-tipovariable(variable)
 
-/* 
-    Es importante saber también que en JS los valores de las variableas no deben ser específicamente tipados sino son por su parte "no tipado"
-    Es decir, que las variables se pueden transformar en variables de cualquier cosa sin necesidad de que sean directamente
-    un string o algo por ese estilo . A menos de que se requieran realizar operaciones matemñaticas las cuales son imposibles de hacer con valor != a numeros o flotantes
-
-    / ::: ¡Que es Typescript! = a grande srasgos es brindada y soportada por microsoft y, le da la capacidad da js de ser altamente tipado lo que permite realizar scripts enfocados a matemáticas y operaciones matemáticas
-*/
+palindromeComprobation('diego');
