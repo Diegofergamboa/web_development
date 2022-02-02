782813db3539cbb60edabb4275460a67479d2a5b
Diego Fernando Ortiz Gamboa <72028938+Diegofergamboa@users.noreply.github.com>
2022-02-01 22:13:16 -0500

    ejercicio de palindromo

--- First_code/app.js
+++ First_code/app.js
@@ -3,14 +3,34 @@
 // return final de comprobacion de si son iguales las palabras
 
 function palindromeComprobation(newWord) {
-	// Convertir el string a array
+	// Convertir el string a array.
 	const separator = (newWord=> {
 		return newWord.split('');
 	});
 	const arrayWord = separator(newWord);
-	console.log(arrayWord[arrayWord.length-1]);
-	// Variable con palabra al revés
-	//console.log(asunderWord);
+	
+    // Variable con el valor del string al revés.
+    let newArray = [];
+    function otherWay() {
+        let contador = arrayWord.length-1;
+        for (let index = 0; index < arrayWord.length; index++) {
+            newArray += arrayWord[contador];
+            contador -= 1;
+        }
+        return newArray;
+    }
+    otherWay(arrayWord)
+	
+    const backward = newArray
+    const forward = arrayWord.join('')
+    
+    // validación de las dos palabras
+
+    if (backward === forward){
+        console.log(true);
+    } else {
+        console.log(false);
+    }
 }
 
-palindromeComprobation('diego');
+palindromeComprobation('juana');
